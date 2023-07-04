da95ac1b4b37ac583a345f968ee0046855fbe97c
sahil <sahiltyagi1204@outlook.com>
2023-07-04 10:23:15 +0530

    more route updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose= require('mongoose')
 const articlesRoute = require('./routes/articles')
-
+const Article = require('./models/articles')
 const ejs = require('ejs')
 
 mongoose.connect("mongodb://127.0.0.1/ArticleP",{useNewUrlParser :true,useUnifiedTopology : true})
@@ -15,21 +15,10 @@ app.use('/articles',articlesRoute)
 
 
 
-app.get('/',(req,res)=>{
-    const articles =[
-        {
-        title : "Test Article",
-        description : "this is an test article",
-        publishAt : new Date()
-    },
-    {
-        title : "Test Article2",
-        description : "this is an test article2",
-        publishAt : new Date()
-    }
-    
-    ]
-    res.render('./articles/index', {articles : articles})
+app.get('/',async (req,res)=>{
+    const articles = await Article.find()
+    res.render('articles/index', {articles:articles})
+
 })
 
 
